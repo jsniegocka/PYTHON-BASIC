88eec33efae9521c0655803ef3214cf51ea8cf21
jsniegocka <jsniegocka@griddynamics.com>
2025-12-18 15:38:08 +0100

    Renamed folders

--- practice/module_3_python_testing/test_read_write.py
+++ practice/module_3_python_testing/test_read_write.py
@@ -1,5 +1,5 @@
 """
-Write tests for 2_python_part_2/task_read_write_2.py task.
+Write tests for module_2_python_part_2/task_read_write.py task.
 To write files during tests use temporary files:
 https://docs.python.org/3/library/tempfile.html
 https://docs.pytest.org/en/6.2.x/tmpdir.html
