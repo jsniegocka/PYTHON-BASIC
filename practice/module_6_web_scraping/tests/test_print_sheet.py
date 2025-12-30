247e9909af0bd438e765518838ffbbfb56aaffb9
jsniegocka <jsniegocka@griddynamics.com>
2025-12-30 13:28:04 +0100

    Added __init__ file to tests folder

---
+++ practice/module_6_web_scraping/tests/test_print_sheet.py
@@ -0,0 +1,37 @@
+from practice.module_6_web_scraping.stock_info_requests import print_sheet
+
+def test_print_sheet_output(capfd):
+    sheet_title = "Sheet"
+    sheet_dict_list = [
+        {
+            "Name": "Pfizer Inc.",
+            "Code": "PFE",
+            "Country": "United States",
+            "Employees": "78500",
+            "CEO Name": "Dr. Albert Bourla D.V.M., DVM, Ph.D.",
+            "CEO Year Born": "1962",
+        },
+        {
+            "Name": "Moderna Inc.",
+            "Code": "MRNA",
+            "Country": "United States",
+            "Employees": "18000",
+            "CEO Name": "Stéphane Bancel",
+            "CEO Year Born": "1972",
+        },
+    ]
+
+    print_sheet(sheet_title, sheet_dict_list[0].keys(), sheet_dict_list)
+
+    captured = capfd.readouterr()
+
+    expected = (
+        "==================================================Sheet===================================================\n"
+        "| Name         | Code | Country       | Employees | CEO Name                             | CEO Year Born |\n"
+        "----------------------------------------------------------------------------------------------------------\n"
+        "| Pfizer Inc.  | PFE  | United States | 78500     | Dr. Albert Bourla D.V.M., DVM, Ph.D. | 1962          |\n"
+        "| Moderna Inc. | MRNA | United States | 18000     | Stéphane Bancel                      | 1972          |\n\n"
+    )
+
+    assert captured.out == expected
+
